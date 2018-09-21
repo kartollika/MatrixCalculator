923a9bad750aae93f81568231718a4307c7dff89
kartollika <maksimow.dmitrij@yandex.ru>
2018-09-21 10:10:55 +0300

    Updated App class due to last changes Removed System.getProperty information

--- app/src/main/java/kartollika/matrixcalc/App.java
+++ app/src/main/java/kartollika/matrixcalc/App.java
@@ -1,36 +1,41 @@
 package kartollika.matrixcalc;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatDelegate;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.MobileAds;
 
+import kartollika.matrixcalc.utilities.AdUtils;
 import kartollika.matrixcalc.utilities.InterstitialShow;
+import kartollika.matrixcalc.utilities.UpdateCheckerBroadcastReceiver;
 
 public class App extends Application {
 
-    // public static Matrix[] matrices = new Matrix[2];
-    // public static Matrix systemMatrix;
     public static final String TARGET_DEVICE_ID = "8161507EB49B3F33630CF2A74D743868";
     public static final int BLOCKING_BANNERS = 90;
     public static final int BLOCKING_INTERSITIALS = 60;
     public static final String email = "maksimow.dmitrij@yandex.ru";
+    private static final String TAG = "APP";
     private static final String APP_ID = "ca-app-pub-9193176037122415~9633966613";
     public static String version;
     public static String CUR_REWARD;
     private static long estimatedTimeRemovingBanners;
     private static long estimatedTimeRemovingInterstitial;
+    //private MatrixManager matrixManager;
+    private static UpdateCheckerBroadcastReceiver updateCheckerBroadcastReceiver;
     public boolean thisSession = false;
     private SharedPreferences preferences;
 
@@ -61,8 +66,7 @@ public class App extends Application {
     public static String getDeviceInfo(WindowManager windowManager) {
         DisplayMetrics metrics = new DisplayMetrics();
         windowManager.getDefaultDisplay().getMetrics(metrics);
-        return "Device Info:" + "\n OS Version: " +
-                System.getProperty("os.version") + "(" + Build.VERSION.CODENAME + ")" +
+        return "Device Info:" +
                 "\n App version/build: " + BuildConfig.VERSION_NAME + "/" + BuildConfig.VERSION_CODE +
                 "\n OS API Level: " + Build.VERSION.SDK_INT +
                 "\n Device: " + Build.DEVICE +
@@ -71,19 +75,19 @@ public class App extends Application {
                 + "x" + metrics.heightPixels;
     }
 
-    public static void writeEmail(Context context) {
-        Resources resources = context.getResources();
+    public static void writeEmail(Activity activity) {
+        Resources resources = activity.getResources();
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setType("plain/text");
         emailIntent = Intent.createChooser(emailIntent, "Choose the client for sending email..");
         emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{App.email});
-       /* emailIntent.putExtra(Intent.EXTRA_SUBJECT,
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT,
                 resources.getString(R.string.email_subject));
         emailIntent.putExtra(Intent.EXTRA_TEXT,
                 resources.getString(R.string.email_text) +
                         "\n\n\n==========================\n" +
-                        App.getDeviceInfo(activity.getWindowManager()));*/
-        context.startActivity(Intent.createChooser(emailIntent, "Sending email..."));
+                        App.getDeviceInfo(activity.getWindowManager()));
+        activity.startActivity(Intent.createChooser(emailIntent, "Sending email..."));
     }
 
     public static void openGooglePlay(Context context) {
@@ -97,39 +101,47 @@ public class App extends Application {
         }
     }
 
+    public static void initUpdaterBroadcastReceiver(Context context) {
+        updateCheckerBroadcastReceiver = new UpdateCheckerBroadcastReceiver(context);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(UpdateCheckerBroadcastReceiver.ACTION_UPDATE_CHECK_FINISHED);
+        intentFilter.addAction(UpdateCheckerBroadcastReceiver.ACTION_UPDATE_CHECK_CONNECTION_ERROR);
+        context.registerReceiver(updateCheckerBroadcastReceiver, intentFilter);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        if (BuildConfig.BUILD_TYPE == "debug") {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        /*AppCompatDelegate.setDefaultNightMode(preferences.getBoolean("isDarkmode", false)
+                ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);*/
+
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
             version = BuildConfig.VERSION_NAME + "_" + BuildConfig.BUILD_TYPE;
         } else {
             version = BuildConfig.VERSION_NAME;
         }
 
-        MobileAds.initialize(getApplicationContext(), APP_ID);
-        //    AdUtils.initResources(this);
-
-
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        AppCompatDelegate.setDefaultNightMode(preferences.getBoolean("isDarkmode", false)
-                ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
-
-        InterstitialShow.CUR_OPERATIONS = preferences.getInt("interstitialCurOperations", 1);
-        setEstimatedTimeBanners(preferences.getLong("bannersEstimatedTime", System.currentTimeMillis()));
-        setEstimatedTimeInterstitial(preferences.getLong("interstitialEstimatedTime", System.currentTimeMillis()));
+        initUpdaterBroadcastReceiver(getApplicationContext());
+        //initAds();
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
-        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = preferences.edit();
+        Log.i(TAG, "onTerminate called");
+        unregisterReceiver(updateCheckerBroadcastReceiver);
+    }
 
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            editor.putBoolean("isDarkmode", true);
-        } else {
-            editor.putBoolean("isDarkmode", false);
-        }
-        editor.apply();
+    private void initAds() {
+        MobileAds.initialize(getApplicationContext(), APP_ID);
+        AdUtils.initResources(this);
+        InterstitialShow.CUR_OPERATIONS = preferences.getInt("interstitialCurOperations", 1);
+        setEstimatedTimeBanners(preferences.getLong("bannersEstimatedTime", System.currentTimeMillis()));
+        setEstimatedTimeInterstitial(preferences.getLong("interstitialEstimatedTime", System.currentTimeMillis()));
+    }
+
+    public static UpdateCheckerBroadcastReceiver getUpdateCheckerBroadcastReceiver() {
+        return updateCheckerBroadcastReceiver;
     }
 }

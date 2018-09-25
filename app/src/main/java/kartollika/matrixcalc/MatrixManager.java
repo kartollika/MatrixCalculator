e0b2752e1a7217e90032187c6f6184ac5ca6dc18
kartollika <maksimow.dmitrij@yandex.ru>
2018-09-26 00:23:30 +0300

    Add singleton MatrixManager

---
+++ app/src/main/java/kartollika/matrixcalc/MatrixManager.java
@@ -0,0 +1,90 @@
+package kartollika.matrixcalc;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import kartollika.matrixcalc.activities.PreferenceActivity;
+import kartollika.matrixmodules.matrix.AugmentedMatrix;
+import kartollika.matrixmodules.matrix.Matrix;
+
+public class MatrixManager {
+
+    private static MatrixManager matrixManager;
+    private SharedPreferences preferenceManager;
+    private Context context;
+    private Matrix matrixA;
+    private boolean isAEdited = false;
+
+    private Matrix matrixB;
+    private boolean isBEdited = false;
+
+    private AugmentedMatrix matrixSystem;
+    private boolean isSystemEdited = false;
+
+    private MatrixManager(Context context) {
+        preferenceManager = PreferenceManager.getDefaultSharedPreferences(context);
+        matrixA = getDefaultMatrix();
+        matrixB = getDefaultMatrix();
+        matrixSystem = getDefaultAugmentedMatrix();
+        this.context = context;
+    }
+
+    public static MatrixManager getInstance(Context context) {
+        if (matrixManager == null) {
+            matrixManager = new MatrixManager(context);
+        }
+        return matrixManager;
+    }
+
+    public Matrix getA() {
+        if (isAEdited) {
+            return matrixA;
+        } else {
+            return getDefaultMatrix();
+        }
+    }
+
+    public void setA(Matrix a) {
+        matrixA = a;
+        isAEdited = true;
+    }
+
+    public Matrix getB() {
+        if (isBEdited) {
+            return matrixB;
+        } else {
+            return getDefaultMatrix();
+        }
+    }
+
+    public void setB(Matrix b) {
+        matrixB = b;
+        isBEdited = true;
+    }
+
+    private Matrix getDefaultMatrix() {
+        return new Matrix(preferenceManager
+                .getInt(PreferenceActivity.KEY_DEFAULT_ROWS, 3),
+                preferenceManager.getInt(PreferenceActivity.KEY_DEFAULT_COLUMNS, 3));
+    }
+
+    private AugmentedMatrix getDefaultAugmentedMatrix() {
+        return new AugmentedMatrix(preferenceManager
+                .getInt(PreferenceActivity.KEY_DEFAULT_ROWS, 3),
+                preferenceManager.getInt(PreferenceActivity.KEY_DEFAULT_COLUMNS, 3));
+    }
+
+    public AugmentedMatrix getMatrixSystem() {
+        if (isSystemEdited) {
+            return matrixSystem;
+        } else {
+            return getDefaultAugmentedMatrix();
+        }
+    }
+
+    public void setSystem(AugmentedMatrix system) {
+        this.matrixSystem = system;
+        isSystemEdited = true;
+    }
+}

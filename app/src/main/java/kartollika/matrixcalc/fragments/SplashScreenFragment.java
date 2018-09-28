ea34cd253c4db5283b23af66f03a0ce046d1ba23
kartollika <maksimow.dmitrij@yandex.ru>
2018-09-28 11:07:02 +0300

    Showing version text on SplashScreenFragment

--- app/src/main/java/kartollika/matrixcalc/fragments/SplashScreenFragment.java
+++ app/src/main/java/kartollika/matrixcalc/fragments/SplashScreenFragment.java
@@ -9,13 +9,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import kartollika.matrixcalc.App;
 import kartollika.matrixcalc.R;
 import kartollika.matrixcalc.activities.MainHubActivity;
 
 public class SplashScreenFragment extends Fragment {
 
     private static final int SPLASH_SCREEN_TIMEOUT = 1500;
+    private TextView textViewVersion;
 
     public static SplashScreenFragment newInstance() {
 
@@ -38,16 +41,23 @@ public class SplashScreenFragment extends Fragment {
         }, SPLASH_SCREEN_TIMEOUT);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textViewVersion.setText(App.getVersion());
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.splash_screen_fragment, container, false);
+        textViewVersion = v.findViewById(R.id.version);
         return v;
     }
 
     private void loadMainActivity() {
-        Intent i = new Intent(getActivity().getApplicationContext(), MainHubActivity.class);
+        Intent i = new Intent(requireContext(), MainHubActivity.class);
         startActivity(i);
-        getActivity().finish();
+        requireActivity().finish();
     }
 }
